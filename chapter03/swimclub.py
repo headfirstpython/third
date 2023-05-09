841b4bdee5e5d9f16f19e0f223adc9d56bf68ee1
Paul Barry <paul.james.barry@gmail.com>
2023-05-09 14:53:59 +0100

    Code for Chapter 3 ready. Formatted with black, too.

--- chapter03/swimclub.py
+++ chapter03/swimclub.py
@@ -4,15 +4,27 @@ FOLDER = "swimdata/"
 
 
 def read_swim_data(filename):
+    """Return swim data from a file.
+
+    Given the name of a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as a tuple.
+    """
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
     with open(FOLDER + filename) as file:
         lines = file.readlines()
         times = lines[0].strip().split(",")
     converts = []
     for t in times:
-        minutes, rest = t.split(":")
-        seconds, hundredths = rest.split(".")
-        converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths))
+        # The minutes value might be missing, so guard against this causing a crash.
+        if ":" in t:
+            minutes, rest = t.split(":")
+            seconds, hundredths = rest.split(".")
+        else:
+            minutes = 0
+            seconds, hundredths = t.split(".")
+        converts.append(
+            (int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths)
+        )
     average = statistics.mean(converts)
     mins_secs, hundredths = str(round(average / 100, 2)).split(".")
     mins_secs = int(mins_secs)
@@ -20,4 +32,4 @@ def read_swim_data(filename):
     seconds = mins_secs - minutes * 60
     average = str(minutes) + ":" + str(seconds) + "." + hundredths
 
-    return swimmer, age, distance, stroke, times, average
+    return swimmer, age, distance, stroke, times, average  # Returned as a tuple.
