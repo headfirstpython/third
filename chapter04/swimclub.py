41b58c45c57a8ce1834480c4ca9675b5aaa2b743
Paul Barry <paul.james.barry@gmail.com>
2023-05-11 21:34:50 +0100

    Chapter 4 code updated and ready to go!

--- chapter04/swimclub.py
+++ chapter04/swimclub.py
@@ -1,6 +1,7 @@
 import statistics
+import hfpy_utils
 
-
+CHARTS = "charts/"
 FOLDER = "swimdata/"
 
 
@@ -23,22 +24,48 @@ def read_swim_data(filename):
         else:
             minutes = 0
             seconds, hundredths = t.split(".")
-        converts.append(
-            (int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths)
-        )
+        converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths))
     average = statistics.mean(converts)
-    mins_secs, hundredths = str(round(average / 100, 2)).split(".")
+    mins_secs, hundredths = f"{(average / 100):.2f}".split(".")
     mins_secs = int(mins_secs)
     minutes = mins_secs // 60
     seconds = mins_secs - minutes * 60
     average = f"{minutes}:{seconds}.{hundredths}"  # Your first f-string.
 
-    return (
-        swimmer,
-        age,
-        distance,
-        stroke,
-        times,
-        average,
-        converts,
-    )  # Returned as a tuple.
+    return swimmer, age, distance, stroke, times, average, converts  # Returned as a tuple.
+
+
+def produce_bar_chart(fn):
+    """Given the name of a swimmer's file, produce a HTML/SVG-based bar chart.
+     
+    Save the chart to the CHARTS folder. Return the path to the bar chart file.
+    """
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
+    from_max = max(converts)
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+    header = f"""<!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>{title}</title>
+                        </head>
+                        <body>
+                            <h3>{title}</h3>"""
+    body = ""
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
+        body = body + f"""
+                            <svg height="30" width="400">
+                                <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+                            </svg>{t}<br />"""
+    footer = f"""
+                            <p>Average time: {average}</p>
+                        </body>
+                    </html>"""
+    page = header + body + footer
+    save_to = f"{CHARTS}{fn.removesuffix('.txt')}.html"
+    with open(save_to, "w") as sf:
+        print(page, file=sf)
+
+    return save_to
