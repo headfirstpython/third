ffc9fe0b510a82c9104e8a261f63075eb73dc0d8
Paul Barry <paul.james.barry@gmail.com>
2023-05-13 16:28:58 +0100

    Added session support to app.py.

--- chapter06/webapp/app.py
+++ chapter06/webapp/app.py
@@ -1,29 +1,41 @@
-from flask import Flask
+from flask import Flask, session
 
 import os
 import swimclub
 
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
 
 
 @app.get("/")
 def index():
     return "This is a placeholder for your webapp's opening page."
+    
+
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        swim_files.remove(".DS_Store")
+        session["swimmers"] = {}
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
 
 
 @app.get("/swimmers")
 def display_swimmers():
-    swim_files = os.listdir(swimclub.FOLDER)
-    swim_files.remove(".DS_Store")
-    swimmers = {}
-    for file in swim_files:
-        name, *_ = swimclub.read_swim_data(file)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
-    return str(sorted(swimmers))
+    populate_data()
+    return str(sorted(session["swimmers"]))
+
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
