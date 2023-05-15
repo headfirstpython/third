3c21f11734ccc3025003be53a73ded85e3d4642c
Paul Barry <paul.james.barry@gmail.com>
2023-05-15 09:51:07 +0100

    The rest of the code for Chapter 9, plus the utility.

---
+++ chapter09/webapp/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, session, render_template, request
+
+import os
+import swimclub
+
+
+app = Flask(__name__)
+app.secret_key = "You will never guess..."
+
+
+@app.get("/")
+def index():
+    return render_template(
+        "index.html",
+        title="Welcome to Swimclub",
+    )
+
+
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        swim_files.remove(".DS_Store")
+        session["swimmers"] = {}
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
+
+
+@app.get("/swimmers")
+def display_swimmers():
+    populate_data()
+    return render_template(
+        "select.html",
+        title="Select a swimmer",
+        url="/showfiles",
+        select_id="swimmer",
+        data=sorted(session["swimmers"]),
+    )
+
+
+@app.post("/showfiles")
+def display_swimmers_files():
+    populate_data()
+    name = request.form["swimmer"]
+    return render_template(
+        "select.html",
+        title="Select an event",
+        url="/showbarchart",
+        select_id="file",
+        data=session["swimmers"][name],
+    )
+
+
+@app.post("/showbarchart")
+def show_bar_chart():
+    file_id = request.form["file"]
+    location = swimclub.produce_bar_chart(file_id, "templates/")
+    return render_template(location.split("/")[-1])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ chapter09/webapp/update_records.py
@@ -0,0 +1,24 @@
+import gazpacho
+import json
+
+URL = "https://en.wikipedia.org/wiki/List_of_world_records_in_swimming"
+RECORDS = (0, 2, 4, 5)
+COURSES = ("LC Men", "LC Women", "SC Men", "SC Women")
+# WHERE = "/home/headfirstpython/webapp/"
+WHERE = ""
+JSONDATA = "records.json"
+
+html = gazpacho.get(URL)
+soup = gazpacho.Soup(html)
+tables = soup.find("table")
+records = {}
+for table, course in zip(RECORDS, COURSES):
+    records[course] = {}
+    for row in tables[table].find("tr")[1:]:
+        columns = row.find("td")
+        event = columns[0].text
+        time = columns[1].text
+        if "relay" not in event:
+            records[course][event] = time
+with open(WHERE + JSONDATA, "w") as jf:
+    json.dump(records, jf)
